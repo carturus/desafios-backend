4c99a39e0d3344775fa8e7767c74ca88dfc9685a
Arturo Cuevas <arturuscv@gmail.com>
2021-09-30 22:47:01 -0500

    42-Desafio-configuracion-servidor-ok

--- 42-configuracion-servidor/config.js
+++ 42-configuracion-servidor/config.js
@@ -4,7 +4,7 @@ const path = require('path');
 dotenv.config({
     path: path.resolve(__dirname, process.env.NODE_ENV + '.env')
   });
-
+console.log(path.resolve(__dirname, process.env.NODE_ENV + '.env'))
 module.exports = {
     NODE_ENV: process.env.NODE_ENV,
     HOST: process.env.HOST || '127.0.0.1',  
