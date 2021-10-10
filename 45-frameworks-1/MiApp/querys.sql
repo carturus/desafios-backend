e12fd53c00396d8dcc503dc551eaefb36a3b11d3
Arturo Cuevas <arturuscv@gmail.com>
2021-10-09 23:27:36 -0500

    45-Desafio-frameworks-1

---
+++ 45-frameworks-1/MiApp/querys.sql
@@ -0,0 +1,9 @@
+create database adonis;
+use adonis;
+
+desc productos;
+
+insert into productos (title, thumbnail, price) values ("Coco", "Torta de Coco", 1234 );
+insert into productos (title, thumbnail, price) values ("Frutilla", "Torta de Frutilla", 2345 );
+
+select * from productos;
